c263550be51957c3ef65678f06e78e6159167ad6
scomeras94 <scomeras@gmail.com>
2024-02-26 23:18:15 +0100

    ADD - Validation Messages

--- sco-telegram-bot-tester-backend/src/modules/telegram-bot/dto/send-message.dto.ts
+++ sco-telegram-bot-tester-backend/src/modules/telegram-bot/dto/send-message.dto.ts
@@ -1,20 +1,21 @@
+import { validationErrorMessages } from '../../../constants/validation-error-messages.constants';
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class SendMessageDto {
     
     @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: validationErrorMessages.SEND_MESSAGE.TOKEN.INVALID_VALUE })
+    @IsNotEmpty({ message: validationErrorMessages.SEND_MESSAGE.TOKEN.NOT_EMPTY })
     token: string;
 
     @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: validationErrorMessages.SEND_MESSAGE.CHAT_ID.INVALID_VALUE })
+    @IsNotEmpty({ message: validationErrorMessages.SEND_MESSAGE.CHAT_ID.NOT_EMPTY })
     chat_id: string;
 
     @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: validationErrorMessages.SEND_MESSAGE.TEXT.INVALID_VALUE })
+    @IsNotEmpty({ message: validationErrorMessages.SEND_MESSAGE.TEXT.NOT_EMPTY })
     text: string;
 }
