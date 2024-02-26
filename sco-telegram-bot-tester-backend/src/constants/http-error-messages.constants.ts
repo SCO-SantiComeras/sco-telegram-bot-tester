6332ac888a212763c15ac2af3071569821a6c533
scomeras94 <scomeras@gmail.com>
2024-02-26 23:39:13 +0100

    UPD - Balance Errors

--- sco-telegram-bot-tester-backend/src/constants/http-error-messages.constants.ts
+++ sco-telegram-bot-tester-backend/src/constants/http-error-messages.constants.ts
@@ -1,4 +1,18 @@
 export const httpErrorMessages = {
+  AUTH: {
+    INVALID_CREDENTIALS: 'Invalid credentials',
+    UNNABLE_USER_TOKEN: 'Unnable to generate user token',
+    UNAUTHORIZED: 'Unauthorized',
+    SESSION_EXPIRED: 'Session expired',
+    USER_NOT_ACTIVED: 'User not actived',
+  },
+  USERS: {
+    USER_NOT_FOUND: 'User not found',
+    USER_ALREADY_EXIST: 'User already exist',
+    EMAIL_ALREADY_EXIST: 'Email already registered',
+    CREATE_USER_ERROR: 'Unnable to create user',
+    UPDATE_USER_ERROR: 'Unnable to update user',
+  },
   TELEGRAM_BOT_TESTER: {
     UNNABLE_CREATE_TELEGRAM_BOT: 'Unnable to create telegram bot',
     BOT_TOKEN_UNAUTHORIZED: 'Bot token unauthorized',
--- sco-telegram-bot-tester-frontend/src/app/shared/http-error/http-error-message-translates.constants.ts
+++ sco-telegram-bot-tester-frontend/src/app/shared/http-error/http-error-message-translates.constants.ts
@@ -1,5 +1,19 @@
 export const httpErrorMessageTranslates = {
   ES: {
+    AUTH: {
+      INVALID_CREDENTIALS: 'Credenciales inválidas',
+      UNNABLE_USER_TOKEN: 'Imposible crear un toke para el usuario',
+      UNAUTHORIZED: 'No autorizado',
+      SESSION_EXPIRED: 'La sesión ha terminado',
+      USER_NOT_ACTIVED: 'El usuario no está activo',
+    },
+    USERS: {
+      USER_NOT_FOUND: 'Usuario no encontrado',
+      USER_ALREADY_EXIST: 'El usuario ya existe',
+      EMAIL_ALREADY_EXIST: 'El email ya ha sido registrado',
+      CREATE_USER_ERROR: 'Imposible crear el usuario',
+      UPDATE_USER_ERROR: 'Imposible actualizar el usuario',
+    },
     TELEGRAM_BOT_TESTER: {
       UNNABLE_CREATE_TELEGRAM_BOT: 'Imposible crear un bot de telegram',
       BOT_TOKEN_UNAUTHORIZED: 'Bot token no autorizado',
@@ -9,12 +23,26 @@ export const httpErrorMessageTranslates = {
     },
   },
   EN: {
+    AUTH: {
+      INVALID_CREDENTIALS: 'Invalid credentials',
+      UNNABLE_USER_TOKEN: 'Unnable to generate user token',
+      UNAUTHORIZED: 'Unauthorized',
+      SESSION_EXPIRED: 'Session expired',
+      USER_NOT_ACTIVED: 'User not actived',
+    },
+    USERS: {
+      USER_NOT_FOUND: 'User not found',
+      USER_ALREADY_EXIST: 'User already exist',
+      EMAIL_ALREADY_EXIST: 'Email already registered',
+      CREATE_USER_ERROR: 'Unnable to create user',
+      UPDATE_USER_ERROR: 'Unnable to update user',
+    },
     TELEGRAM_BOT_TESTER: {
       UNNABLE_CREATE_TELEGRAM_BOT: 'Unnable to create telegram bot',
       BOT_TOKEN_UNAUTHORIZED: 'Bot token unauthorized',
       BOT_TOKEN_NOT_FOUND: 'Bot token not found',
       CHAT_ID_NOT_FOUND: 'Chat id not found',
-      CHAT_GROUP_DELETE: 'Group chat was deleted',
+      CHAT_GROUP_DELETE: 'Group chat was deleted'
     },
   }
 };
