0eefeba1200733f1e1e790ae479374a6dcc6f218
scomeras94 <scomeras@gmail.com>
2024-02-27 12:30:59 +0100

    ADD - Header

--- sco-telegram-bot-tester-frontend/src/app/app.component.ts
+++ sco-telegram-bot-tester-frontend/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { SpinnerConstants } from './shared/spinner/spinner.constants';
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 import { ConfigService } from "./shared/config/config.service";
 import { WebSocketService } from "./websocket/websocket.service";
 import { SpinnerService } from "./shared/spinner/spinner.service";
 import { ConfigConstants } from './shared/config/config.constants';
+import { CacheService } from './shared/cache/cache.service';
+import { CacheConstants } from './shared/cache/cache.constants';
+import { TranslateService } from './shared/translate/translate.service';
+import { ResolutionService } from './shared/resolution/resolution.service';
+import { ResolutionConstants } from './shared/resolution/resolution.constants';
 
 @Component({
   selector: 'app-root',
@@ -13,18 +18,33 @@ import { ConfigConstants } from './shared/config/config.constants';
 export class AppComponent {
   
   public title: string;
+  public viewMode: string;
+
   public readonly spinnerConstants = SpinnerConstants;
   public readonly configConstants = ConfigConstants;
+  public readonly cacheConstants = CacheConstants;
+  public readonly resolutionConstants = ResolutionConstants;
 
   constructor(
     private readonly configService: ConfigService,
     private readonly websocketsService: WebSocketService,
     public readonly spinnerService: SpinnerService,
+    public readonly cacheService: CacheService,
+    private readonly translateService: TranslateService,
+    public readonly resolutionService: ResolutionService,
   ) {
     if (this.configService.getData(this.configConstants.TITLE)) {
       this.title = this.configService.getData(this.configConstants.TITLE) || 'sco-telegram-bot-tester';
     }
 
+    this.viewMode = this.resolutionService.getMode();
+
+    this.cacheService.setElement(this.cacheConstants.TITLE, this.translateService.getTranslate('label.header.cache.title'))
     this.websocketsService.connect();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize($event) {
+    this.viewMode = this.resolutionService.getMode($event.target.innerWidth);
   }
 }
