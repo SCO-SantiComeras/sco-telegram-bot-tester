508404629122a14d510a089682d3ef1828fff8bb
scomeras94 <scomeras@gmail.com>
2024-02-27 01:23:29 +0100

    ADD - Emailer subject title env variable

--- sco-telegram-bot-tester-backend/src/configuration/configuration-emailer.ts
+++ sco-telegram-bot-tester-backend/src/configuration/configuration-emailer.ts
@@ -3,4 +3,5 @@ export const configurationEmailer = registerAs("emailer", () => ({
   address: process.env.EMAIL_EMAILER,
   password: process.env.PASSWORD_EMAILER,
   service: process.env.SERVICE_EMAILER,
+  subjectTitle: process.env.SUBJECT_TITLE_EMAILER,
 }));
