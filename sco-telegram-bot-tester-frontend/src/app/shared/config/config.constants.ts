300ec2b203cbd483875bd32a18ac6c1e95ea66a3
scomeras94 <scomeras@gmail.com>
2024-02-27 13:03:14 +0100

    ADD - tokenExpirationPasswordRecovery Config Variable

--- sco-telegram-bot-tester-frontend/src/app/shared/config/config.constants.ts
+++ sco-telegram-bot-tester-frontend/src/app/shared/config/config.constants.ts
@@ -1,4 +1,5 @@
 export const ConfigConstants = {
     TITLE: 'title',
     RESET_FORM_AFTER_SUCCESS_REQUEST: 'resetFormAfterSuccessReset',
+    TOKEN_EXPIRATION_PWD_RECOVERY: 'tokenExpirationPasswordRecovery',
 }
