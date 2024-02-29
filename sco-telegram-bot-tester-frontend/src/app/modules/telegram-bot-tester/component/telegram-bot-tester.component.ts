8042e27b9c53bea341fd07679d39a933cbea3088
scomeras94 <scomeras@gmail.com>
2024-02-29 21:25:30 +0100

    UPD - Telegram bot tester

--- sco-telegram-bot-tester-frontend/src/app/modules/telegram-bot-tester/component/telegram-bot-tester.component.ts
+++ sco-telegram-bot-tester-frontend/src/app/modules/telegram-bot-tester/component/telegram-bot-tester.component.ts
@@ -11,14 +11,12 @@ import { ToastService } from 'src/app/shared/toast/toast.service';
 import { Select, Store } from '@ngxs/store';
 import { TelegramBotTesterMockConstants } from 'src/app/shared/constants/telegram-bot-tester.mock.constants';
 import { environment } from 'src/environments/environment';
-import { CacheConstants } from 'src/app/shared/cache/cache.constants';
-import { ConfigConstants } from 'src/app/shared/config/config.constants';
 import { ConfigService } from 'src/app/shared/config/config.service';
-import { SendMessageGroup } from '../../telegram-bot-results/store/telegram-bot-results.actions';
-import { TelegramBotResultsState } from '../../telegram-bot-results/store/telegram-bot-results.state';
 import { AuthState } from '../../auth/store/auth.state';
 import { Observable } from 'rxjs';
 import { User } from '../../auth/model/user';
+import { SendMessageGroup } from '../store/telegram-bot-tester.actions';
+import { TelegramBotTesterState } from '../store/telegram-bot-tester.state';
 
 @Component({
   selector: 'app-telegram-bot-tester',
@@ -26,9 +24,6 @@ import { User } from '../../auth/model/user';
   styleUrls: ['./telegram-bot-tester.component.scss']
 })
 export class TelegramBotTesterComponent implements OnInit, OnDestroy {
-
-  public cacheConstants = CacheConstants;
-  public configConstants = ConfigConstants;
 
   public sendMessageForm: FormGroup;
   public formErrors: FormsError[];
@@ -48,10 +43,9 @@ export class TelegramBotTesterComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.loggedUser$.subscribe((loggedUser: User) => {
+      this.loggedUser = undefined;
       if (loggedUser && loggedUser._id) {
         this.loggedUser = loggedUser;
-      } else {
-        this.loggedUser = undefined;
       }
     });
     
@@ -61,7 +55,7 @@ export class TelegramBotTesterComponent implements OnInit, OnDestroy {
       text: new FormControl('', [Validators.required]),
     });
 
-    if (!environment.production && this.configService.getData(this.configConstants.START_MOCKED_VALUES)) {
+    if (!environment.production && this.configService.getData(this.configService.configConstants.START_MOCKED_VALUES)) {
       this.setMockedValues();
     }
   }
@@ -80,7 +74,7 @@ export class TelegramBotTesterComponent implements OnInit, OnDestroy {
       return;
     }
 
-    if (this.configService.getData(this.configConstants.ONLY_GROUP_ID) && sendMessage.chat_id[0] != '-') {
+    if (this.configService.getData(this.configService.configConstants.ONLY_GROUP_ID) && sendMessage.chat_id[0] != '-') {
       sendMessage.chat_id = `-${sendMessage.chat_id}`;
     }
 
@@ -95,23 +89,23 @@ export class TelegramBotTesterComponent implements OnInit, OnDestroy {
     this.spinnerService.showSpinner();
     this.store.dispatch(new SendMessageGroup({ sendMessage: sendMessage })).subscribe({
       next: () => {
-        const success: boolean = this.store.selectSnapshot(TelegramBotResultsState.success);
+        const success: boolean = this.store.selectSnapshot(TelegramBotTesterState.success);
         if (!success) {
           this.spinnerService.hideSpinner();
-          this.toastService.addErrorMessage(this.store.selectSnapshot(TelegramBotResultsState.errorMsg));
+          this.toastService.addErrorMessage(this.store.selectSnapshot(TelegramBotTesterState.errorMsg));
           return;
         }
 
-        if (this.configService.getData(this.configConstants.RESET_FORM_AFTER_SUCCESS_REQUEST)) {
+        if (this.configService.getData(this.configService.configConstants.RESET_FORM_AFTER_SUCCESS_REQUEST)) {
           this.onCLickClean();
         }
         this.spinnerService.hideSpinner();
-        this.toastService.addSuccessMessage(this.store.selectSnapshot(TelegramBotResultsState.successMsg));
+        this.toastService.addSuccessMessage(this.store.selectSnapshot(TelegramBotTesterState.successMsg));
         return;
       }, 
       error: () => {
         this.spinnerService.hideSpinner();
-        this.toastService.addErrorMessage(this.store.selectSnapshot(TelegramBotResultsState.errorMsg));
+        this.toastService.addErrorMessage(this.store.selectSnapshot(TelegramBotTesterState.errorMsg));
       }
     })
   }
@@ -120,22 +114,34 @@ export class TelegramBotTesterComponent implements OnInit, OnDestroy {
     this.formErrors = [];
 
     if (!sendMessage.token) {
-      this.formErrors.push({ formControlName: 'token', error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.token') });
+      this.formErrors.push({ 
+        formControlName: 'token', 
+        error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.token') 
+      });
     }
 
     if (sendMessage.token) {
       const splitToken: string[] = sendMessage.token.split(':');
       if (!splitToken  || splitToken && splitToken.length != 2) {
-        this.formErrors.push({ formControlName: 'token', error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.token.format') });
+        this.formErrors.push({ 
+          formControlName: 'token', 
+          error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.token.format') 
+        });
       }
     }
 
     if (!sendMessage.chat_id) {
-      this.formErrors.push({ formControlName: 'chat_id', error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.chat_id') });
+      this.formErrors.push({ 
+        formControlName: 'chat_id', 
+        error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.chat_id') 
+      });
     }
 
     if (!sendMessage.text) {
-      this.formErrors.push({ formControlName: 'text', error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.text') });
+      this.formErrors.push({ 
+        formControlName: 'text', 
+        error: this.translateService.getTranslate('label.telegram-bot.component.form.validate.text') 
+      });
     }
 
     this.sendMessageForm = this.formsService.setErrors(this.sendMessageForm, this.formErrors);
