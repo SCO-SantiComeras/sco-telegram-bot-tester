adb077998630347a647bf96b0d778075a1487a9d
scomeras94 <scomeras@gmail.com>
2024-02-29 15:58:04 +0100

    ADD - Shared Module Import

--- sco-telegram-bot-tester-backend/src/modules/telegram-bot/telegram-bot.module.ts
+++ sco-telegram-bot-tester-backend/src/modules/telegram-bot/telegram-bot.module.ts
@@ -3,11 +3,13 @@ import { TelegramBotService } from './telegram-bot.service';
 import { TelegramBotController } from './telegram-bot.controller';
 import { TelegramBotResultsModule } from '../telegram-bot-results/telegram-bot-results.module';
 import { UsersModule } from '../users/users.module';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
   imports: [
     UsersModule,
     TelegramBotResultsModule,
+    SharedModule,
   ],
   controllers: [
     TelegramBotController,
