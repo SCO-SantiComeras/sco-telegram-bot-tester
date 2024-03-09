b6a29f40b4e287bf497adb1029685beb54681ada
scomeras94 <scomeras@gmail.com>
2024-03-09 13:02:43 +0100

    ADD - App configuration Fe Env Variables

--- sco-telegram-bot-tester-backend/src/configuration/configuration-app.ts
+++ sco-telegram-bot-tester-backend/src/configuration/configuration-app.ts
@@ -4,6 +4,8 @@ export const configurationApp = registerAs('app', () => ({
   env: process.env.ENV_APP,
   port: parseInt(process.env.PORT_APP, 10 || 3000),
   host: process.env.HOST_APP,
+  frontendPort: parseInt(process.env.FRONTEND_PORT_APP, 10 || 4200),
+  frontendHost: process.env.FRONTEND_HOST_APP,
   production: process.env.PRODUCTION_APP == 'true',
   population: process.env.POPULATION_APP == 'true',
   onlyOneAdmin: process.env.ONLY_ONE_ADMIN_APP == 'true',
