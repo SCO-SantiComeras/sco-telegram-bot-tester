4fd904383b02cc9e7fcd4b0de0a80a6105acfe65
scomeras94 <scomeras@gmail.com>
2024-03-09 12:59:07 +0100

    UPD - Env variables

--- sco-telegram-bot-tester-frontend/src/environments/environment.prod.ts
+++ sco-telegram-bot-tester-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   name: 'prod',
   production: true,
-  host: '82.223.202.120',
-  apiUrl: `http://82.223.202.120:3000/api/v1`,
-  socketUrl: `http://82.223.202.120:8080`,
+  host: 'scoapps.es',
+  apiUrl: `http://scoapps.es:3000/api/v1`,
+  socketUrl: `ws://scoapps.es:3100`,
 };
--- sco-telegram-bot-tester-frontend/src/environments/environment.ts
+++ sco-telegram-bot-tester-frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: false,
   host: 'localhost',
   apiUrl: `http://localhost:3000/api/v1`,
-  socketUrl: `ws://localhost:8080`,
+  socketUrl: `ws://localhost:3100`,
 };
